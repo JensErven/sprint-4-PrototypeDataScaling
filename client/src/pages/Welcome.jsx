b97bbc6dd25d5518c45644d6c2743636ee2885a2
Jens Erven <jens.erven@student.ehb.be>
2023-12-04 13:36:51 +0100

    feat: framer-motion added to welcome page

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { motion } from "framer-motion";
+
 import {
   faArrowLeft,
   faArrowRight,
@@ -7,8 +9,24 @@ import {
   faPlaneCircleCheck,
 } from "@fortawesome/free-solid-svg-icons";
 import plants_background from "../assets/plants.jpg";
+import { useNavigate } from "react-router-dom";
 
 const Welcome = () => {
+  const navigate = useNavigate();
+
+  const textVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1 } },
+  };
+  const buttonAnimation = {
+    hidden: { y: 100 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: { duration: 1, delay: 1, type: "spring" },
+    },
+  };
+
   return (
     <div className="bg-[#11493e] relative flex items-center flex-col text-left justify-between h-screen">
       <div className="h-screen">
@@ -18,24 +36,35 @@ const Welcome = () => {
           alt="Description"
           className="w-screen  object-cover bg-gradient-to-b  to-blue-[#11493e] h-1/2"
         />
-        <div className="p-4 flex flex-col gap-4">
+        <motion.div
+          className="p-6 flex flex-col gap-4"
+          initial="hidden"
+          animate="visible"
+          variants={textVariants}
+        >
           <h1 className="">Keep your plants alive</h1>
           <p>
             Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt,
             nihil? Esse tempore blanditiis autem ea. Deserunt aspernatur
             dolore...
           </p>
-        </div>
+        </motion.div>
       </div>
 
-      <button className="w-screen absolute bottom-0 border-t-[1px] border-slate-600 h-[8%] justify-center items-center flex flex-row  gap-2 rounded-t-3xl bg-[#ffffff] text-stone-800">
+      <motion.button
+        initial="hidden"
+        animate="visible"
+        variants={buttonAnimation}
+        className="w-screen border-t-[1px] border-slate-600 h-[8%] justify-center items-center flex flex-row  gap-2 rounded-t-3xl bg-[#ffffff] text-stone-800"
+        onClick={() => navigate("/login")}
+      >
         <p className="text-[#010001] font-extrabold">Ga Verder</p>
         <FontAwesomeIcon
           icon={faArrowRight}
           fontSize={25}
           className="text-[#010001]"
         />
-      </button>
+      </motion.button>
     </div>
   );
 };
